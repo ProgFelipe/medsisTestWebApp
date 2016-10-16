551cdc7e8137b59d6237bee96af9ac47aee7d520
Felipe Gutiérrez <felipegutierrez@Felipes-Air.dummy.porta.siemens.net>
2016-10-16 18:25:18 -0500

    autocomplete search by username

--- app/module.js
+++ app/module.js
@@ -1,5 +1,10 @@
 //Create controllers
 app.controller('LoadUsersController', function($scope, $http) {
+  /*
+  *http://jsonplaceholder.typicode.com/users?username=Bret
+  *This service doesnt provide function to autocomplete
+  *requires the full username to show data
+  */
   $http.get('http://jsonplaceholder.typicode.com/users/').
       then(function(response) {
           $scope.customers = response.data;
@@ -31,4 +36,47 @@ app.controller('PaginationDemoCtrl', function ($scope, $log) {
   };
   $scope.bigCurrentPage = 1;
 });
+
+//http://angular-ui.github.io/bootstrap/#/typeahead
+app.controller('TypeaheadCtrl', function($scope, $filter, $log) {
+
+  var _selected;
+  $scope.selected;
+  $scope.autocompleteDetail = false;
+  $scope.filterParams = {
+              order: 'username'
+              };
+
+  $scope.onSelectedCustomer = function($item, $model, $label){
+    $log.log($item);
+    $scope.autocompleteDetailVisibility = true;
+    $scope.selected = $item;
+  };
+
+  $scope.onHideDetail = function(){
+    $scope.autocompleteDetailVisibility = false;
+  }
+  // Any function returning a promise object can be used to load values asynchronously
+  $scope.getLocation = function(val) {
+    var filteredData = $filter('filter')($scope.customers, {username: val});
+    filteredData = $filter('orderBy')(filteredData, $scope.filterParams.order);
+    var usersNames;
+    return filteredData;
+    //For service asynchronously autocomplete
+    /*return $http.get('http://jsonplaceholder.typicode.com/users', {
+      params: {
+        username: val,
+      }
+    }).then(function(response){
+      if(response.data.results != null){
+        return response.data.results.map(function(item){
+        return item.formatted_address;
+      });
+      }else{
+        return response.data;
+      }
+    });*/
+  };
+});
+
 //End Bootstrap modules
